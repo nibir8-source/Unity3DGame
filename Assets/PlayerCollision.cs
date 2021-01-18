1bb6eee25d3da87424d6a9f843ae0b099b93bf0c
Nibir <nibir2893@gmail.com>
2021-01-19 03:16:01 +0530

    hello

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Player_movement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/Player_movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
